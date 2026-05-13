64911263b4f026eeba70e15527cb031d4de0848e
Alpeshsorte <alpeshsorte4@gmail.com>
2026-05-13 21:26:28 +0530

    order function add

--- orders/orders.js
+++ orders/orders.js
@@ -1,6 +1,56 @@
-let orderform=document.getElementById("orderform")
-orderform.addEventListener("click", form)
+let tbody = document.getElementById("orderTableBody");
 
-function form(){
+// fetch order data
+async function getOrders() {
+
+    try {
+
+        let res = await fetch("http://localhost:3000/orders");
+
+        let data = await res.json();
+
+        displayOrders(data);
+
+    } 
     
+    catch(error) {
+
+        alert("error in fetching orders");
+    }
+}
+
+getOrders();
+
+
+// display data in table
+function displayOrders(data) {
+
+    tbody.innerHTML = "";
+
+    data.forEach((el) => {
+
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
+
+            <td>${el.id}</td>
+            <td>${el.customerName}</td>
+            <td>${el.mobile}</td>
+            <td>${el.product}</td>
+            <td>${el.quantity}</td>
+            <td>₹ ${el.totalAmount}</td>
+            <td>${el.address}</td>
+            <td>${el.paymentStatus}</td>
+            <td>${el.orderStatus}</td>
+            <td>${el.createdAt}</td>
+            <td>${el.status}</td>
+
+            <td>
+                <button class="editBtn">Edit</button>
+                <button class="deleteBtn">Delete</button>
+            </td>
+        `;
+
+        tbody.append(tr);
+    });
 }
--- products.js
+++ products.js
@@ -1,12 +1,9 @@
 
 let productform = document.getElementById("productform")
-
 productform.addEventListener("submit", getproductdata)
 
 function getproductdata(event) {
-
     event.preventDefault()
-
     let name = document.querySelector("#productform #name").value
     let brand = document.querySelector("#productform #brand").value
     let category = document.querySelector("#productform #category").value
@@ -26,16 +23,12 @@ function getproductdata(event) {
         description,
         status
     }
-
     product_post(obj)
-
     productform.reset()
 }
 
 async function product_post(data) {
-
     try {
-
         await fetch(`http://localhost:3000/products`, {
             method: "POST",
             body: JSON.stringify(data),
@@ -43,13 +36,10 @@ async function product_post(data) {
                 "Content-Type": "application/json"
             }
         })
-
         getproduct()
 
     } catch (error) {
-
         console.log("Something went wrong")
-
     }
 
 }
@@ -67,12 +57,10 @@ async function getproduct() {
     } catch (error) {
 
         console.log("Error in get product")
-
     }
-
 }
-
 getproduct()
+
 
 function displayCards(arr) {
 
@@ -255,258 +243,263 @@ async function updateproduct(event) {
             
     //     }
 
+//   final code of searcing the product 
 
-let select = document.getElementById("type")
+// let select = document.getElementById("type")
 
-let productsearch = document.getElementById("productsearch")
+// let productsearch = document.getElementById("productsearch")
 
-let search_Btn = document.getElementById("search_Btn")
+// let search_Btn = document.getElementById("search_Btn")
 
-let container = document.getElementById("productContainer")
+// let container = document.getElementById("productContainer")
 
-// SEARCH BUTTON
+// // SEARCH BUTTON
 
-search_Btn.addEventListener("click", dosearch)
+// search_Btn.addEventListener("click", dosearch)
 
-// SEARCH FUNCTION
+// // SEARCH FUNCTION
 
-async function dosearch() {
+// async function dosearch() {
 
-    try {
+//     try {
 
-        let select_val = select.value.trim()
+//         let select_val = select.value.trim()
 
-        let product_val = productsearch.value.trim().toLowerCase()
+//         let product_val = productsearch.value.trim().toLowerCase()
 
-        let response = await fetch("http://localhost:3000/products")
-        let actualData = await response.json()
+//         let response = await fetch("http://localhost:3000/products")
+//         let actualData = await response.json()
 
-        console.log(actualData)
+//         console.log(actualData)
 
-        let filteredData = actualData.filter((el) => {
+//         let filteredData = actualData.filter((el) => {
 
-            if(!el[select_val]){
+//             if(!el[select_val]){
 
-                return false
-            }
+//                 return false
+//             }
 
-            return el[select_val]
-            .toString()
-            .toLowerCase()
-            .includes(product_val)
+//             return el[select_val]
+//             .toString()
+//             .toLowerCase()
+//             .includes(product_val)
 
-        })
+//         })
 
-        container.innerHTML = ""
-        if(filteredData.length === 0){
-            container.innerHTML = "<h1>No Product Found</h1>"
-            return
-        }
-        mapdata(filteredData)
-    }
-    catch(error){
-        console.log(error)
-        alert("Search Error")
-    }
-}
+//         container.innerHTML = ""
+//         if(filteredData.length === 0){
+//             container.innerHTML = "<h1>No Product Found</h1>"
+//             return
+//         }
+//         mapdata(filteredData)
+//     }
+//     catch(error){
+//         console.log(error)
+//         alert("Search Error")
+//     }
+// }
 
 // Category 
+// let category = document.getElementById("Category");
+// category.addEventListener("change", getData);
+// async function getData() {
+//     try {
+//         let sel_val = category.value;
+//         let data = await fetch(`http://localhost:3000/products?category=${sel_val}`);
+//         let ac_Data = await data.json();
+//         displayCards(ac_Data);
+//     } 
+//     catch(error) {
+//         alert("error in category filter");
+//     }
+// }
+// getData();
 
-let Category=document.getElementById("Category")
-Category.addEventListener("click", dofilter)
+//    final code of category
 
-async function dofilter(){
-    try{
-        let cat_val=document.getElementById("Category").value
-
-        let data=await fetch(
-            `http://localhost:3000/products?Category=${cat_val}`
-        )
-        let actualData=await data.json()
-
-        if(actualData === 0){
-            alert("data not filter")
-        }
-        mapdata(actualData)
-
-    }catch(error){
-        alert("error in filter data")
+/*
+let category = document.getElementById("Category");
+category.addEventListener("change", getData);
+async function getData(arr) { 
+    let selectedValue = category.value;
+    let url = "http://localhost:3000/products";
+    // filter using URL
+    if (selectedValue !== "all") {
+        url = `http://localhost:3000/products?category=${selectedValue}`;
     }
+    let res = await fetch(url);
+    let data = await res.json();
+    displayCards(data);
 }
+getData();
+*/
 
-// sort thing price
 
-let pricerange=document.getElementById("price_range")
-pricerange.addEventListener("change",dosort)
+// final code for price sort
+/*
+let pricerange = document.getElementById("price");
+pricerange.addEventListener("change", dosort);
 
 async function dosort() {
-
-    try{
-
-        let sort_val=pricerange.value
-
-        let data=await fetch(
-            `http://localhost:3000/products?_sort=${sort_val}`
-
-        )
-
-        let actualData=await data.json()
-        mapdata(actualData)
-    }
-    catch(error){
-        alert("data not sort")
-    }
-}
-  
-//   stock 
-let stock=document.getElementById("Stock")
-stock.addEventListener("click", dofilterstock)
-
-async function dofilterstock(){
-    try{
-        let stock_val=document.getElementById("Stock").value
-
-        let data=await fetch(
-            `http://localhost:3000/products?Stock=${stock_val}`
-        )
-        let actualData=await data.json()
-
-        if(actualData === 0){
-            alert("data not filter")
+    try {
+        let sort_val = pricerange.value;
+        let url = "http://localhost:3000/products";
+        // ₹0 - ₹5000
+        if (sort_val === "₹0-₹5000") {
+            url = `http://localhost:3000/products?price_gte=0&price_lte=5000`;
         }
-        mapdata(actualData)
-
-    }catch(error){
-        alert("error in filter data")
+        // ₹5000 - ₹20000
+        else if (sort_val === "₹5000-₹20000") {
+            url = `http://localhost:3000/products?price_gte=5000&price_lte=20000`;
+        }
+        // ₹20000+
+        else if (sort_val === "₹20000+") {
+            url = `http://localhost:3000/products?price_gte=20000`;
+        }
+        let data = await fetch(url);
+        let actualData = await data.json();
+        displayCards(actualData);
+    } 
+    catch(error) {
+        alert("data not sort");
     }
 }
+dosort();
+*/
+  
+// final code for stock chack  
+/*
+let stock = document.getElementById("Stock");
+stock.addEventListener("change", dofilterstock);
 
-// brand 
+async function dofilterstock() {
+    try {
+        let stock_val = stock.value;
+        let url = "http://localhost:3000/products";
+        // filter by status
+        if (stock_val !== "all") {
+            url = `http://localhost:3000/products?status=${stock_val}`;
+        }
+        let res = await fetch(url);
+        let data = await res.json();
+        displayCards(data);
+    } 
+    catch(error) {
+        alert("error in filter data");
+    }
+}
+dofilterstock();
+*/
 
+
+// final code of the brand 
+/*
 let brand=document.getElementById("brand")
 brand.addEventListener("click", dofilterbrand)
 
 async function dofilterbrand(){
     try{
-        let brand_val=document.getElementById("brand").value
+        let brand_val=brand.value
 
-        let data=await fetch(
-            `http://localhost:3000/products?Category=${brand_val}`
-        )
-        let actualData=await data.json()
+        let url="http://localhost:3000/products";
 
-        if(actualData === 0){
-            alert("data not filter")
+        if(brand_val !== "all"){
+            url=`http://localhost:3000/products?brand=${brand_val}`;
         }
-        mapdata(actualData)
+        let res=await fetch(url)
+
+        let data=await res.json()
+        displayCards(data)
 
     }catch(error){
         alert("error in filter data")
     }
 }
+*/
 
+// final code for sorting but not work
+/*
+let sorting = document.getElementById("sorting");
+sorting.addEventListener("change", dosorting);
 
-
-
-function mapdata(arr){
-
-    container.innerHTML = ""
-
-    arr.forEach((el) => {
-
-
-         let card = document.createElement("div")
-        card.classList.add("card")
-
-        let image = document.createElement("img")
-        image.src = el.image
-
-        let cardContent = document.createElement("div")
-        cardContent.classList.add("cardContent")
-
-        let name = document.createElement("h2")
-        name.textContent = `Name : ${el.name}`
-
-        let brand = document.createElement("h3")
-        brand.textContent = `Brand : ${el.brand}`
-
-        let category = document.createElement("h3")
-        category.textContent = `Category : ${el.category}`
-
-        let price = document.createElement("h4")
-        price.textContent = `Price : ₹${el.price}`
-
-        let quantity = document.createElement("h4")
-        quantity.textContent = `Quantity : ${el.quantity}`
-
-        let description = document.createElement("p")
-        description.textContent = el.description
-
-        let status = document.createElement("h4")
-        status.textContent = el.status
-
-        let buttonBox = document.createElement("div")
-        buttonBox.classList.add("buttonBox")
-
-        let editBtn = document.createElement("button")
-        editBtn.textContent = "Edit"
-        editBtn.classList.add("editBtn")
-
-        let deleteBtn = document.createElement("button")
-        deleteBtn.textContent = "Delete"
-        deleteBtn.classList.add("deleteBtn")
-
-        // edit functionalty create is below
-         editBtn.addEventListener("click", () => {
-
-            document.getElementById("product_id").value = el.id
-
-            document.getElementById("update_name").value = el.name
-
-            document.getElementById("update_brand").value = el.brand
-
-            document.getElementById("update_category").value = el.category
-
-            document.getElementById("update_price").value = el.price
-
-            document.getElementById("update_quantity").value = el.quantity
-
-            document.getElementById("update_image").value = el.image
-
-            document.getElementById("update_description").value = el.description
-
-            document.getElementById("update_status").value = el.status
-
-        })
-
-        // delete functionalty create is 
-        deleteBtn.addEventListener("click", async () => {
-
-            await fetch(`http://localhost:3000/products/${el.id}`, {
-                method: "DELETE"
-            })
-
-            getproduct()
-
-        })
-
-        buttonBox.append(editBtn, deleteBtn)
-
-        cardContent.append(
-            name,
-            brand,
-            category,
-            price,
-            quantity,
-            description,
-            status,
-            buttonBox
-        )
-
-        card.append(image, cardContent)
-
-        container.append(card)
-
-    })
-
+async function dosorting() {
+    try {
+        let sorting_val = sorting.value;
+        let url = "http://localhost:3000/products";
+        // Price Low to High
+        if (sorting_val === "price-asc") {
+            url = `http://localhost:3000/products?_sort=price&_order=asc`;
+        }
+        // Price High to Low
+        else if (sorting_val === "price-desc") {
+            url = `http://localhost:3000/products?_sort=price&_order=desc`;
+        }
+        // Name A-Z
+        else if (sorting_val === "name-asc") {
+            url = `http://localhost:3000/products?_sort=name&_order=asc`;
+        }
+        // Name Z-A
+        else if (sorting_val === "name-desc") {
+            url = `http://localhost:3000/products?_sort=name&_order=desc`;
+        }
+        let res = await fetch(url);
+        let data = await res.json();
+        displayCards(data);
+    } 
+   
+    catch(error) {
+       alert("error in the sorting part");
+    }
 }
+dosorting();
+
+*/
+
+//  final code pagination  
+
+let currentPage = 1;
+let limit = 6;
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+// fetch data
+async function getData() {
+
+    try {
+
+        let res = await fetch(
+            `http://localhost:3000/products?_page=${currentPage}&_per_page=${limit}`
+        );
+
+        let data = await res.json();
+
+        // json-server pagination data
+        displayCards(data.data);
+
+    } 
+    
+    catch(error) {
+
+        alert("error in pagination");
+    }
+}
+
+getData();
+
+nextBtn.addEventListener("click", () => {
+
+    currentPage++;
+
+    getData();
+});
+
+prevBtn.addEventListener("click", () => {
+
+    if(currentPage > 1){
+
+        currentPage--;
+
+        getData();
+    }
+});
+
